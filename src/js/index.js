6679973e06b190bd8964e92f1db814f399d188b5
bobbyallen1099 <38939673+bobbyallen1099@users.noreply.github.com>
2019-11-24 23:22:58 +0000

    Add about me and work

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,9 @@
 import '../scss/app.scss';
 import homeBanner from '../images/home-banner.svg';
+import aboutMe from '../images/about-me.svg';
+import johnFowlers from '../images/john-fowlers.jpg';
+import marfleet from '../images/marfleet.jpg';
+import haywardMoon from '../images/hayward-moon.jpg';
 
 var $ = require("jquery");
 // var Pjax = require("pjax");
