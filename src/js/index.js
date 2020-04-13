ef7ed0bba45ee0524eb6f29eaf12cc9ca3a1d0f8
Bobby Allen <bobbyallen1099@gmail.com>
2020-04-13 14:22:43 +0100

    Website improvements

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,15 @@
 import '../scss/app.scss';
 import homeBanner from '../images/home-banner.svg';
 import aboutMe from '../images/about-me.svg';
+import aboutMeCode from '../images/about-me-code.svg';
 import johnFowlers from '../images/john-fowlers.jpg';
 import marfleet from '../images/marfleet.jpg';
 import haywardMoon from '../images/hayward-moon.jpg';
 import bobbyAllen from '../images/bobby-allen.jpg';
+import 'bootstrap';
 
 var $ = require("jquery");
+var popper = require("popper.js");
 var Pjax = require("pjax");
 var Barba = require("barba.js");
 
@@ -23,29 +26,29 @@ var FadeTransition = Barba.BaseTransition.extend({
         .all([this.newContainerLoading, this.fadeOut()])
         .then(this.fadeIn.bind(this));
     },
-  
+
     fadeOut: function() {
       return $(this.oldContainer).animate({opacity: 0 }, 200, 'swing').promise();
     },
-  
+
     fadeIn: function() {
       var _this = this;
       var $el = $(this.newContainer);
-  
+
       $(this.oldContainer).hide();
-  
+
       $el.css({
         visibility : 'visible',
         opacity : 0,
       });
-  
+
       $el.animate({ opacity: 1 }, 200, 'swing', function() {
         _this.done();
       });
     }
   });
-  
-  
+
+
   Barba.Pjax.getTransition = function() {
     return FadeTransition;
   };
